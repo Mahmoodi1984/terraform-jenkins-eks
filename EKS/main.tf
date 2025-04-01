d75f0a7c1eec04db5e6eb747ddff11740c2ab0b5
Abdul Samad Mahmoodi <159003888+Mahmoodi1984@users.noreply.github.com>
2025-04-01 11:37:42 +0200

    Update main.tf

--- EKS/main.tf
+++ EKS/main.tf
@@ -33,7 +33,7 @@ module "eks" {
   version = "~> 20.31"
 
   cluster_name    = "my-eks-cluster"
-  cluster_version = "1.31"
+  cluster_version = "1.22"  # Update to a valid Kubernetes version (1.22, 1.23, or 1.24)
 
   cluster_endpoint_public_access = true
 
@@ -48,6 +48,13 @@ module "eks" {
       instance_type = ["t2.small"]
     }
   }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
 
   tags = {
     Environment = "dev"
